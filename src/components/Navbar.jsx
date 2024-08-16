399820b3978b6e3118b9f1f35608c0cd1f725e4a
unknown <shinzawasif03@gmail.com>
2024-08-16 16:56:26 +0500

    React Website (1) initial content added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div className='bg-white shadow-lg'>
+        <div className="container">test</div>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Navbar
